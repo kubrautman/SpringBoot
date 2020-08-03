9a0949f28a1755a1b5c0b10bbda3f2b4462459e2
busra utman <busautmn@outlook.com>
2020-08-03 21:59:43 +0300

    Configure Hibernate ,Devtools,application.properties

--- src/main/java/com/bsrutmn/SpringBootSample/api/ProductController.java
+++ src/main/java/com/bsrutmn/SpringBootSample/api/ProductController.java
@@ -10,5 +10,15 @@ public class ProductController {
  public  String get() {
 	 return "Laptop";
  }
+	 
+	 @RequestMapping(value = "/products", method = RequestMethod.GET)
+ public  String getProducts() {
+	 return "Laptop2";
+ }
+	 
+	 @RequestMapping(value = "/customer", method = RequestMethod.GET)
+ public  String getCustomer() {
+	 return "customer";
+ }
 	
 }
